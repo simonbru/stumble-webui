52cadfe4e5bfa422fd822ff7ebf1cac3c91ca67c
Simon Brulhart <simon@brulhart.me>
2018-06-02 13:17:20 +0200

    Bring some configurability for URL of backend API

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -3,35 +3,36 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { Sound } from './sound';
+import { environment } from '../environments/environment';
 
+
+const BACKEND_URL: String = environment.backendUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  private backendUrl = 'http://localhost:3003';
-
   constructor(
     private http: HttpClient
   ) { }
 
   getSounds(): Observable<Sound[]> {
-    return this.http.get(`${this.backendUrl}/sounds`).pipe(
+    return this.http.get(`${BACKEND_URL}/sounds`).pipe(
       map(res => res['sounds']),
     );
   }
 
   playSound(soundId: String): Observable<Object> {
     return this.http.post(
-      `${this.backendUrl}/commands/play/invoke`,
+      `${BACKEND_URL}/commands/play/invoke`,
       { message: soundId }
     );
   }
 
   speak(text: String): Observable<Object> {
     return this.http.post(
-      `${this.backendUrl}/commands/tts/invoke`,
+      `${BACKEND_URL}/commands/tts/invoke`,
       { message: text }
     );
   }
